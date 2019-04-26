f59f38c9d98ca0ac9425858f270989f1d79c79d3
grshen80 <grshen80@gmail.com>
2019-04-26 12:31:06 -0500

    Adding database schemas

---
+++ src/database.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+	name: String,
+	email: { type: String, required: true },
+	pictureUrl: String,
+	likedSongs: [{ songId: String, likedDate: { type: Date, default: new Date() }],
+	history: [{
+		songs: [{ songId: String, searchDate: { type: Date, default: new Date() }],
+		comparisons: [{ songId1: String, songId2: String, compareDate: { type: Date, default: new Date() }]
+	}],
+	recommended: [String]
+});
+
+var SongSchema = new mongoose.Schema({
+	songTitle: { type: String, required: true },
+	artist: { type: String, required: true },
+	genre: String,
+	albumName: String,
+	albumImgUrl: String,
+	lyrics: String,
+	relatedSongs: [String],
+	sentiment: Number,
+	BPM: Number
+});
+
+var userModel = mongoose.model('User', UserSchema);
+var songModel = mongoose.model('Song', SongSchema);
+
+module.exports = {
+	userModel: userModel,
+	songModel: songModel
+}	
