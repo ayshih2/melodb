b3deaf314aae4665f3e39d929dd2a2ca899b986a
ayshih2 <ayshih2@illinois.edu>
2019-04-27 16:20:49 -0500

    Created two animated search bars

--- src/Components/Compare/Compare.jsx
+++ src/Components/Compare/Compare.jsx
@@ -8,32 +8,50 @@ class Compare extends Component {
 
     };
 
-    this.searchRef = React.createRef();
+    this.leftSearchRef = React.createRef();
+    this.rightSearchRef = React.createRef();
     this._onBlur = this._onBlur.bind(this);
-    this._onFocus = this._onFocus.bind(this);
+    this._onLeftFocus = this._onLeftFocus.bind(this);
+    this._onRightFocus = this._onRightFocus.bind(this);
   }
 
 	_onBlur() {
-		var elem = this.searchRef.current;
-		if (elem.value.length == 0) {
-			elem.parentElement.classList.remove('active');
+		var leftSearchElem = this.leftSearchRef.current;
+		if (leftSearchElem.value.length === 0) {
+			leftSearchElem.parentElement.classList.remove('active');
+		}
+
+		var rightSearchElem = this.rightSearchRef.current;
+		if (rightSearchElem.value.length === 0) {
+			rightSearchElem.parentElement.classList.remove('active');
 		}
 	}
 
-	_onFocus() {
-    var elem = this.searchRef.current;
-    elem.parentElement.classList.add('active');
+	_onLeftFocus() {
+    var leftSearchElem = this.leftSearchRef.current;
+    leftSearchElem.parentElement.classList.add('active');
+	}
+
+	_onRightFocus() {
+		var rightSearchElem = this.rightSearchRef.current;
+    rightSearchElem.parentElement.classList.add('active');
 	}
 
 	/* icon to search bar animation from https://codepen.io/sebastianpopp/pen/myYmmy with tweaks to make it for react */
   render() {
     return (
 			<div className="cntr">
-				<div className="cntr-innr">
-	  			<label className="search" htmlFor="inpt_search" onFocus={this._onFocus} onBlur={this._onBlur}>
-						<input ref={this.searchRef} id="inpt_search" type="text" />
+				<span id="leftSearch">
+	  			<label className="search" htmlFor="left_inpt_search" onFocus={this._onLeftFocus} onBlur={this._onBlur}>
+						<input ref={this.leftSearchRef} id="left_inpt_search" type="text" />
 					</label>
-				</div>
+				</span>
+
+				<span id="rightSearch">
+					<label className="search" htmlFor="right_inpt_search" onFocus={this._onRightFocus} onBlur={this._onBlur}>
+						<input ref={this.rightSearchRef} id="right_inpt_search" type="text" />
+					</label>
+				</span>
 			</div>
     );
   }
