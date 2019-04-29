f74a9c73a9bdeef6f2e0f524e8f269d4e5be1239
grshen80 <grshen80@gmail.com>
2019-04-28 22:58:54 -0500

    Change the way api calls are for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,30 +3,69 @@ var Song = models.songModel;
 var User = models.userModel;
 
 module.exports = function(router) {
-	var userIdRoute = router.route('/user/:id');
+	var userRoute = router.route('/user');
 	// var tempWorldRoute = router.route('/world');
 	// var songRoute = router.route('/song');
 
-	userIdRoute.get((req, res) => {
-		User.findById(req.params.id, (err, res_user) => {
-			if (!res_user) {
-				res.status(404).send({
-                    message: "User not found",
-                    data: {}
-                });
-			} else if (err) {
-				res.status(500).send({
-                    message: "Server error",
-					data: {},
-					error: err
-                });
+	userRoute.get((req, res) => {
+		if (Object.keys(req.query).length == 1) {
+			User.findById(req.query.id, (err, res_user) => {
+				if (!res_user) {
+					res.status(404).send({
+	                    message: "User not found",
+	                    data: {}
+	                });
+				} else if (err) {
+					res.status(500).send({
+	                    message: "Server error",
+						data: {},
+						error: err
+	                });
+				} else {
+					res.status(200).send({
+						message: "OK",
+						data: res_user
+					});
+				}
+			});
+		} else if (Object.keys(req.query).length == 2) {
+			if (req.query.type == "recommended") {
+				//stuff goes here
+			} else if (req.query.type == "history") {
+				//stuff goes here
+			} else if (req.query.type == "liked") {
+				User.findById(req.query.id, (err, res_user) => {
+					if (!res_user) {
+						res.status(404).send({
+		                    message: "User not found",
+		                    data: {}
+		                });
+					} else if (err) {
+						res.status(500).send({
+		                    message: "Server error",
+							data: {},
+							error: err
+		                });
+					} else {
+						res.status(200).send({
+							message: "OK",
+							data: res_user.likedSongs
+						});
+					}
+				});
 			} else {
-				res.status(200).send({
-					message: "OK",
-					data: res_user
+				res.status(400).send({
+					message: "This is not a valid GET type for a user.",
+					data: {}
 				});
 			}
-		});
+
+		} else {
+			res.status(400).send({
+				message: "This is not a valid request.",
+				data: {}
+			});
+		}
 		// res.status(200).send({ express: 'Hello From Express' });
 	});
 
