13b868efe94589ce6886448cba6cf2d93c58175c
Jeremy <jeremyyshu@gmail.com>
2019-04-30 21:05:33 -0500

    Added Routers

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -7,27 +7,43 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      first: true
     };
-
+    this.out = React.createRef();
     this.searchRef = React.createRef();
     this.inputChangehandler = this.inputChangehandler.bind(this);
+    this.changeHeight = this.changeHeight.bind(this);
+  }
+
+  changeHeight(elem) {
+    if (this.state.first) {
+      elem.style.transform = '';
+      elem.style.transition = '';
+      this.out.current.style.height = '100%';
+    }
+    this.setState({
+      first: false
+    });
   }
 
   inputChangehandler(e) {
-    var elem = this.searchRef.current;
-    elem.style.transform = 'translateY(-46vh)';
-    elem.style.transition = "all 1000ms";
+    if (this.state.first) {
+      var elem = this.searchRef.current;
+      elem.style.transform = 'translateY(-40vh)';
+      elem.style.transition = "all 1000ms";
+    }
     this.setState({
       value: e.target.value
-    });
+    })
+    setTimeout(this.changeHeight, 800, elem);
   }
 
   render() {
     return (
       <div>
-        <div ref={this.searchRef} className='searchBar'>
-          <Input onChange={this.inputChangehandler} value={this.state.value} />
+        <div className='searchBar' ref={this.out}>
+          <div ref={this.searchRef} ><Input onChange={this.inputChangehandler} value={this.state.value} /></div>
         </div>
         <Display query={this.state.value} />
       </div>
